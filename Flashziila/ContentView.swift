08335233871634020f00116cc71b89e60ace4350
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-07-12 16:43:50 +0100

    Initial Commit

---
+++ Flashziila/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Flashziila
+//
+//  Created by Mehmet Alp Sönmez on 12/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var offset = CGSize.zero
+    @State private var isDragging = false
+    
+    var body: some View {
+        let dragGesture = DragGesture()
+            .onChanged { value in
+                offset = value.translation
+            }
+            .onEnded { _ in
+                withAnimation {
+                    offset = .zero
+                    isDragging = false
+                }
+            }
+        
+        let pressGesture = LongPressGesture()
+            .onEnded { valur in
+                withAnimation {
+                    isDragging = true
+                }
+            }
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        Circle()
+            .fill(.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flashziila/FlashziilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashziilaApp.swift
+//  Flashziila
+//
+//  Created by Mehmet Alp Sönmez on 12/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashziilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
