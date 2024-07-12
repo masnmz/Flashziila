9d10216f259da724ee50cf7ee0f4916fc01be593
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-07-12 17:05:48 +0100

    allowHitTesting, contentShape

--- Flashziila/ContentView.swift
+++ Flashziila/ContentView.swift
@@ -8,35 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
+   
     
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
-            }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
-                }
-            }
-        
-        let pressGesture = LongPressGesture()
-            .onEnded { valur in
-                withAnimation {
-                    isDragging = true
-                }
-            }
-        let combined = pressGesture.sequenced(before: dragGesture)
-        
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
+        VStack {
+            Text("Hello")
+            
+            Spacer()
+                .frame(height: 100)
+            
+            Text("world")
+        }
+        .contentShape(.rect)
+        .onTapGesture {
+            print("Vstak is tapped")
+        }
     }
 }
 
