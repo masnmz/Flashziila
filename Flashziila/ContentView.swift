d16d4bf61c318ee710f89b60ef374820b17050d8
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-07-15 23:11:08 +0100

    Timer

--- Flashziila/ContentView.swift
+++ Flashziila/ContentView.swift
@@ -8,21 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-   
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+   @State private var counter = 0
     
     var body: some View {
-        VStack {
-            Text("Hello")
-            
-            Spacer()
-                .frame(height: 100)
-            
-            Text("world")
-        }
-        .contentShape(.rect)
-        .onTapGesture {
-            print("Vstak is tapped")
-        }
+        Text("Hello World!")
+            .onReceive(timer) { time in
+                if counter == 5 {
+                    cancelTimer()
+                } else {
+                    print("The time is now \(time)")
+                    counter += 1
+                }
+                
+                
+                   
+            }
+    }
+    
+    func cancelTimer() {
+        timer.upstream.connect().cancel()
     }
 }
 
