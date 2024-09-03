393e56d1384ffa07d204b650a0f33f1b345256bc
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-09-03 17:10:27 +0100

    Adding and Deleting Cards

---
+++ Flashziila/EditCards.swift
@@ -0,0 +1,18 @@
+//
+//  EditCards.swift
+//  Flashziila
+//
+//  Created by Mehmet Alp Sönmez on 03/09/2024.
+//
+
+import SwiftUI
+
+struct EditCards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCards()
+}
